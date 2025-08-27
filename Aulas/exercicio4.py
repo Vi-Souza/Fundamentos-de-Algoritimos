5b5bf8c103e53788b4c33e5fd9ac9a3295248581
Vi-Souza <vitorsr.souza02@gmail.com>
2025-08-27 00:55:26 +0000

    int por float e 2 casas dps da virgula

--- Aulas/exercicio4.py
+++ Aulas/exercicio4.py
@@ -1,13 +1,13 @@
 valor = int(input("Digite o valor da hora: "))
-horas = int(input("Digite a quantidade de horas trabalhadas no mes: "))
+horas = float(input("Digite a quantidade de horas trabalhadas no mes: "))
 sal_bruto = valor * horas
 desc_IR = sal_bruto * 0.11
 desc_INSS = sal_bruto * 0.08
 desc_Sind = sal_bruto * 0.05
 sal_liq = sal_bruto - (desc_INSS + desc_IR + desc_Sind)
 print(f"""
-Salario bruto: R$ {sal_bruto}
-- IR (11%): R$ {desc_IR}
-- INSS (8%): R$ {desc_INSS}
-- Sindicato (5%): R$ {desc_Sind}
-Salario liquido: R$ {sal_liq}""")
+Salario bruto: R$ {sal_bruto:.2f}
+- IR (11%): R$ {desc_IR:.2f}
+- INSS (8%): R$ {desc_INSS:.2f}
+- Sindicato (5%): R$ {desc_Sind:.2f}
+Salario liquido: R$ {sal_liq:.2f}""")

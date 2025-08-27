555069daffeb379affc07fcdb343c59b281363b3
Vi-Souza <vitorsr.souza02@gmail.com>
2025-08-27 00:44:48 +0000

    formatando

--- Aulas/exercicio3.py
+++ Aulas/exercicio3.py
@@ -3,4 +3,4 @@ horas = int(input("Digite a quantidade de horas: "))
 min = int(input("Digite a quantidade de minutos: "))
 seg = int(input("Digite a quantidade de segundos: "))
 total_seg = (dias*86400)+(horas*3600)+(min*60)+seg
-print(f"A quantidade total de segundos é: {total_seg} seg")
+print(f"A quantidade total de segundos é: {total_seg:,d}")
--- Aulas/exercicio4.py
+++ Aulas/exercicio4.py
@@ -5,8 +5,9 @@ desc_IR = sal_bruto * 0.11
 desc_INSS = sal_bruto * 0.08
 desc_Sind = sal_bruto * 0.05
 sal_liq = sal_bruto - (desc_INSS + desc_IR + desc_Sind)
-print(f"Salario bruto: R$ {sal_bruto}")
-print(f"- IR (11%): R$ {desc_IR}")
-print(f"- INSS (8%): R$ {desc_INSS}")
-print(f"- Sindicato (5%): R$ {desc_Sind}")
-print(f"Salario liquido: R$ {sal_liq}")
+print(f"""
+Salario bruto: R$ {sal_bruto}
+- IR (11%): R$ {desc_IR}
+- INSS (8%): R$ {desc_INSS}
+- Sindicato (5%): R$ {desc_Sind}
+Salario liquido: R$ {sal_liq}""")

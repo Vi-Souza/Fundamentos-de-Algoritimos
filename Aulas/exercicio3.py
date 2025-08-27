a2739154ba66abe579b402f8782b8c9f0c8248b1
Vi-Souza <vitorsr.souza02@gmail.com>
2025-08-27 00:08:41 +0000

    exercicio 3

---
+++ Aulas/exercicio3.py
@@ -0,0 +1,6 @@
+dias = int(input("Digite a quantidade de dias: "))
+horas = int(input("Digite a quantidade de horas: "))
+min = int(input("Digite a quantidade de minutos: "))
+seg = int(input("Digite a quantidade de segundos: "))
+total_seg = (dias*86400)+(horas*3600)+(min*60)+seg
+print(f"A quantidade total de segundos é: {total_seg} seg")

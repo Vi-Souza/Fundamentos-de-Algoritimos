9840287107ba55b68720beb7430ff8cb4dc4276b
Vi-Souza <vitorsr.souza02@gmail.com>
2025-08-29 00:55:29 +0000

    exercicio 3 finalizado

--- Exercicios/Exercicio_3/exercicio1.py
+++ Exercicios/Exercicio_3/exercicio1.py
@@ -1,5 +1,5 @@
-n1 = float(input("digite o primeiro numero"))
-n2 = float(input("digite o segundo numero"))
+n1 = float(input("digite o primeiro numero: "))
+n2 = float(input("digite o segundo numero: "))
 
 if n1>n2:
     print("maior: %.2f" %n1)
